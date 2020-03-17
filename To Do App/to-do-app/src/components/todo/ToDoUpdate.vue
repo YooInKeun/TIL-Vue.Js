6b4c1e8f144fb25057ae8c6a0eec6524ac5dd064
YooInKeun <keun0390@naver.com>
2020-03-17 17:49:44 +0900

    [ADD] 수정 페이지 완료

---
+++ To Do App/to-do-app/src/components/todo/ToDoUpdate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="container">
+        *제 목: <input type="text" v-model="title"><br><br>
+        *내 용: <input type="text" v-model="content"> 
+    </div>
+    <br>
+    <button @click="addToDo()">수정 완료</button>
+    <router-link to="/todo/list">메인 페이지</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toDoUpdate',
+  data () {
+    return {
+        title: null,
+        content: null
+    }
+  },
+  created: function () {
+      this.title = this.$route.query.todo_id;
+  },
+  methods: {
+      addToDo() {
+          console.log(this.title);
+          console.log(this.content);
+          // 통신 부분
+          
+          alert("수정 완료 되었습니다");
+          this.title = null;
+          this.content = null;
+          
+          this.$router.push({
+            name: "toDoList"
+          });
+      }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ To Do App/to-do-app/views/todo/ToDoUpdateView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <page-header>
+      <template slot="title">To Do Update</template>
+    </page-header>
+    <to-do-update></to-do-update>
+  </div>
+</template>
+
+<script>
+import PageHeader from "../../src/components/common/PageHeader.vue";
+import ToDoUpdate from "../../src/components/todo/ToDoUpdate.vue";
+
+export default {
+  components: {
+    PageHeader,
+    ToDoUpdate
+  }
+};
+</script>
