4da91076659a9473c4c85ab31c939d72d3f2e0e8
YooInKeun <keun0390@naver.com>
2020-03-17 16:13:59 +0900

    [ADD] 메인 조회 페이지 완료

--- To Do App/to-do-app/src/components/todo/ToDoList.vue
+++ To Do App/to-do-app/src/components/todo/ToDoList.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    {{ msg }}
+    <div v-for="(todo, index) in todos" :key="index">
+      <ul>
+        <li><h2>할 일 {{ index }} 번</h2></li>
+       <h3>제목: {{ todo.title }}</h3>
+       <h3>{{ todo.content }}</h3>
+      </ul>
+      <br>
+    </div>
+    <button>등록</button>
   </div>
 </template>
 
@@ -9,36 +17,38 @@ export default {
   name: 'ToDoList',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todos: [
+        {
+          id: 1,
+          title: "운동",
+          content: "수영하기"
+        },
+        {
+          id: 2,
+          title: "공부",
+          content: "수학"
+        },
+        {
+          id: 3,
+          title: "여가 생활",
+          content: "보드"
+        },
+        {
+          id: 4,
+          title: "장 보기",
+          content: "고기"
+        }
+      ]
     }
   },
     created: function () {
       this.fetch_all_data();
   },
   methods: {
-      fetch_all_data: function () {
-
-          var json = {
-              params: {
-                  col_int: 239
-              }
-          };
-
-          axios.get('http://192.168.0.86:8580//chunjae/typecheck/oracle', json)
-              .then((res) => {
-                  console.log("GET SUCESS", res);
-                  result = JSON.parse(res.data.data);
-                  console.log(result);
-
-                  for (key in result) {
-                      console.log('key : ' + key + ' / ' + 'type : ' + typeof(result[key]) + ' / ' + 'value:' + result[key]);
-                  };
-              })
-              .catch((err) => {
-                  console.log("GET FAIL", err);
-              });
-        },
+    fetch_all_data: function () {
+      // 데이터를 받았다고 가정
     },
+  },
 }
 </script>
 
