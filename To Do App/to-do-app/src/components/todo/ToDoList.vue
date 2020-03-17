6d3f428ee4db6818a784276059663fd5ae7a63b6
YooInKeun <keun0390@naver.com>
2020-03-17 15:58:56 +0900

    [ADD] axios 통신 테스트 완료

--- To Do App/to-do-app/src/components/todo/ToDoList.vue
+++ To Do App/to-do-app/src/components/todo/ToDoList.vue
@@ -11,7 +11,34 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+    created: function () {
+      this.fetch_all_data();
+  },
+  methods: {
+      fetch_all_data: function () {
+
+          var json = {
+              params: {
+                  col_int: 239
+              }
+          };
+
+          axios.get('http://192.168.0.86:8580//chunjae/typecheck/oracle', json)
+              .then((res) => {
+                  console.log("GET SUCESS", res);
+                  result = JSON.parse(res.data.data);
+                  console.log(result);
+
+                  for (key in result) {
+                      console.log('key : ' + key + ' / ' + 'type : ' + typeof(result[key]) + ' / ' + 'value:' + result[key]);
+                  };
+              })
+              .catch((err) => {
+                  console.log("GET FAIL", err);
+              });
+        },
+    },
 }
 </script>
 
