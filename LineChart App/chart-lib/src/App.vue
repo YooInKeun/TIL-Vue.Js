118013cd637fb9844f99507e61971c7531e72644
Yoo In Keun <keun0390@naver.com>
2020-03-31 23:22:59 +0900

    [DELETE] comment

--- LineChart App/chart-lib/src/App.vue
+++ LineChart App/chart-lib/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Chart.js</h1>
-    <bar-chart v-on:refresh:chart="refreshChart" v-bind:propsdata="chartDataSet"></bar-chart>
+    <bar-chart></bar-chart>
     <line-chart></line-chart>
   </div>
 </template>
@@ -17,20 +17,11 @@ export default {
   },
   data() {
     return {
-      // chartDataSet: [],
     }
   },
   methods: {
-    // refreshChart() {
-    //   this.chartDataSet = [10, 20];
-    // }
   },
-  // 비즈니스 로직은 최대한 상위 컴포넌트에서 사용
-  // 하위 컴포넌트는 상위 컴포넌트에서 받은 데이터를 받아오기만 하게끔
   created() {
-    // axios.get('charts/line/1')
-    //   .then(response => this.chartDataSet = response.data)
-    //   .catch(error => console.log(error));
   }
 }
 </script>
