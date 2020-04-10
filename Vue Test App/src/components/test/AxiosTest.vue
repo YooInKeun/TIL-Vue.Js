1c18c18a3a2f8c139a86bae3f442a4957675a663
YooInKeun <keun0390@naver.com>
2020-04-10 11:23:17 +0900

    [ADD] Component Axios Test

---
+++ Vue Test App/src/components/test/AxiosTest.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="axios">
+    <h1>Component Axios 테스트</h1>
+    <button @click="selectUser">API 호출</button>
+    <div>
+      콘솔을 확인하세요
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+axios.defaults.baseURL = 'http://anylogic.iptime.org:8580/';
+
+export default {
+  components: {},
+  data() {
+    return {
+      users: []
+    }
+  },
+  computed: {},
+  methods: {
+    selectUser() {
+      var tmpCondition =
+        "?age=&end_dt=&pageCon=10&pageNum=1&start_dt=&tel=&userSortDirection=ASC&userSortItem=user_seq&user_id=&user_name=&view_cnt=";
+      axios
+        .get("chunjae/user/selectUser/" + tmpCondition)
+        .then(response => {
+          console.log("GET RES", response);
+          this.users = response.data.data.rows;
+        })
+        .catch(error => {
+          console.log("GET ERR", error);
+        })
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.child {
+  background-color: cyan;
+}
+</style>
--- Vue Test App/src/views/test/TestListView.vue
+++ Vue Test App/src/views/test/TestListView.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <parent> </parent>
+    <br><br><br>
+    <axios-test> </axios-test>
   </div>
 </template>
 
 <script>
 import Parent from "../../components/test/Parent.vue";
+import AxiosTest from "../../components/test/AxiosTest.vue";
 
 export default {
   components: {
-    Parent
+    Parent,
+    AxiosTest
   },
   data() {
     return {}
