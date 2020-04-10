493f40768f12816541e2ac4057dd8c011f32009c
YooInKeun <keun0390@naver.com>
2020-04-10 11:14:39 +0900

    [ADD] 자식 -> 부모 emit 이벤트 발생(파라미터 포함)

--- Vue Test App/src/components/test/Child.vue
+++ Vue Test App/src/components/test/Child.vue
@@ -1,17 +1,32 @@
 <template>
-  <div></div>
+  <div class="child">
+    <h1>자식 컴포넌트</h1>
+    <button @click="fromChildToParent">Emit(자식 -> 부모)</button>
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return {}
+    return {
+      childData: {
+        from: "child",
+        to: "parent"
+      }
+    }
   },
   computed: {},
-  methods: {}
+  methods: {
+    fromChildToParent() {
+      this.$emit("child", this.childData);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.child {
+  background-color: cyan;
+}
 </style>
--- Vue Test App/src/components/test/Parent.vue
+++ Vue Test App/src/components/test/Parent.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-      dddddddddddd
-      <child></child>
+  <div class="parent">
+    <h1>부모 컴포넌트</h1>
+    <h2>자식으로부터 받은 데이터: {{ childData.from }} {{ childData.to }}</h2>
+    <child v-on:child="fromChildToParent"></child>
   </div>
 </template>
 
@@ -10,15 +11,28 @@ import Child from "./Child.vue"
 
 export default {
   components: {
-      Child
+    Child
   },
   data() {
-    return {}
+    return {
+      childData: {
+        from: "",
+        to: ""
+      }
+    }
   },
   computed: {},
-  methods: {}
+  methods: {
+    fromChildToParent(childData) {
+      alert("자식 컴포넌트 이벤트 발생");
+      this.childData = childData;
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.parent {
+  background-color: cornflowerblue;
+}
 </style>
