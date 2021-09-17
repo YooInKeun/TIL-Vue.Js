f6a944fb861ad94a46efad4ba0995ddce9cde481
YooInKeun <keun0390@naver.com>
2021-09-18 00:08:54 +0900

    chore: add v-for practice

--- vue-tutorial/src/components/Tutorial.vue
+++ vue-tutorial/src/components/Tutorial.vue
@@ -6,6 +6,11 @@
     <h2 v-if="seen" @mouseover="disableSeen" @mouseleave="enableSeen">
       마우스를 올리면 메세지가 사라집니다!
     </h2>
+    <ol>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -15,7 +20,12 @@ export default {
   data: () => {
     return {
       message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다!',
-      seen: true
+      seen: true,
+      todos: [
+        { id: 1, text: '@mouseleave가 왜 안 먹히지?' },
+        { id: 2, text: 'vue 문법 한 사키을 돌기' },
+        { id: 3, text: '졸린데 오늘은 이만 잘까?' }
+      ]
     };
   },
   methods: {
