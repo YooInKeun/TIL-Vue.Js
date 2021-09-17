6ee5b48e89c3d9fe9253f3475083eb94bc0f1dce
YooInKeun <keun0390@naver.com>
2021-09-17 23:32:38 +0900

    chore: add v-bind practice

--- vue-tutorial/src/components/Tutorial.vue
+++ vue-tutorial/src/components/Tutorial.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    ㅎㅇ
+    <h2 v-bind:title="message">
+      마우스를 올리면 잠시 후에 메세지가 보입니다!
+    </h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Tutorial"
+  name: "Tutorial",
+  data: () => {
+    return {
+      message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다!'
+    };
+  }
 }
 </script>
 
