dadd8a193f6766d2dddde0d7c911e20302d26c68
YooInKeun <keun0390@naver.com>
2021-09-18 12:35:35 +0900

    chore: add @click practice

--- vue-tutorial/src/components/Tutorial.vue
+++ vue-tutorial/src/components/Tutorial.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 v-bind:title="message">
+    <h2 v-bind:title="timestampMessage">
       마우스를 올리면 잠시 후에 메세지가 보입니다!
     </h2>
     <h2 v-if="seen" @mouseover="disableSeen" @mouseleave="enableSeen">
@@ -11,6 +11,8 @@
         {{ todo.text }}
       </li>
     </ol>
+    <h2>{{ message }}</h2>
+    <button @click="reverseMessage">메세지 뒤집기</button>
   </div>
 </template>
 
@@ -19,13 +21,14 @@ export default {
   name: "Tutorial",
   data: () => {
     return {
-      message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다!',
+      timestampMessage: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다!',
       seen: true,
       todos: [
         { id: 1, text: '@mouseleave가 왜 안 먹히지?' },
         { id: 2, text: 'vue 문법 한 cycle 돌기' },
         { id: 3, text: '졸린데 오늘은 이만 잘까?' }
-      ]
+      ],
+      message: '버튼을 클릭하면 메세지가 뒤집어집니다!'
     };
   },
   methods: {
@@ -34,6 +37,9 @@ export default {
     },
     disableSeen() {
       this.seen = false;
+    },
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
     }
   }
 }
